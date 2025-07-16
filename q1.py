0f5b068693251503a73db5bef9da93f3e8c12db8
Moneesh12 <168181257+Moneesh12@users.noreply.github.com>
2025-07-16 12:44:19 +0530

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+
+
+rf = pd.read_excel("Lab Session Data.xlsx", sheet_name="Purchase data", usecols="B:E", header=0)
+
+
+
+#A1)
+
+A = rf.iloc[:,:-1].values
+
+C = rf.iloc[:,-1].values.reshape(-1,1)
+
+ATA = A.T.dot(A)
+ATC = A.T.dot(C)
+
+rankA = np.linalg.matrix_rank(A)
+
+A1 = np.linalg.pinv(A)
+
+
+
+X = np.linalg.inv(ATA).dot(ATC)
+print("A martix:\n",A)
+print("C matrix:\n",C)
+print("X matrix:\n",X)
+print("dimension of A",A.shape)
+print("dimension of C",C.shape)
+print("rank of A",rankA)
+print("pseudo inverse:",A1)
