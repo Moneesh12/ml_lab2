7ac843cdaf1ba446f5b111a432ca70b425a47a45
Moneesh12 <168181257+Moneesh12@users.noreply.github.com>
2025-07-19 23:43:43 +0530

    Update q2.py

--- q2.py
+++ q2.py
@@ -3,9 +3,9 @@
 import pandas as pd
 
 rf = pd.read_excel("Lab Session Data.xlsx", sheet_name="Purchase data", usecols="A:E")
-
+#we are going through the payment amount
 for index, row in rf.iterrows():
-    if row['Payment (Rs)'] > 200:#we
+    if row['Payment (Rs)'] > 200:#we are applying a condition to find if rich or poor
         print("RICH")
     else:
         print("POOR")
